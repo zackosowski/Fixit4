aac596a72e40bbee351bf0368feb391599bf6934
zackosowski <ZACHARYO@MYSTMA.ORG>
2020-10-23 19:56:22 +0000

    Created assignment

--- JavaTemplate/src/app/App.java
+++ JavaTemplate/src/app/App.java
@@ -1,7 +1,27 @@
+/*
+FIXIT 4
+
+The program below takes a word submitted by the user and counts the number oif letters in the word.
+The problem is that it does not store this data and does not output the answer to the user.
+Change this program so that it outputs the number of letters in the word to the user.
+
+*/
+
 package app;
+
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a word");
+        String word = input.nextLine();
+
+        for (int i = 0; i < word.length(); i++) {
+
+        }
+
     }
 }
